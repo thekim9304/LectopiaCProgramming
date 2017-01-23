60357063e46e98cc9923118e976425987cb8e59f
DESKTOP-BITTDHG\kimteahyoung <th_k9304@naver.com>
2017-01-23 19:46:06 +0900

    2017.01.22 빙고게임(수정 - 함수 정리)

--- MyCoding/bingo_Game_2017.01.22.cpp
+++ MyCoding/bingo_Game_2017.01.22.cpp
@@ -18,6 +18,7 @@ void deleteAllocI(char**, int);
 
 void playIndividual();
 void playConfrontation();
+int printResult(int, int, int);
 
 int inputNumber(char**, int*, int, int*);
 void inputNumberC(char **, char **, int, int);
@@ -66,6 +67,150 @@ int mainMenu()
 }
 
 
+void playIndividual()
+{
+	int i, j, input, ret = 1, countLine = 0, num;
+	static int count[1000], t[1000];
+	char **myBingo;
+
+	printf("# 빙고판의 가로,세로 크기를 입력해주세요(양수값 입력) : ");
+	scanf("%d", &input);
+	printf("\n");
+
+	myBingo = (char**)malloc(sizeof(char*) * input);
+	assert(myBingo != NULL);
+
+	makeAllocI(myBingo, input);
+
+	//배열 초기화
+	for (i = 0; i < (input * input); i++)
+	{
+		count[i] = 0;
+		t[i] = 0;
+	}
+
+	//난수 생성
+	makeRandS(myBingo, count, input);
+	printf("\n");
+
+	//실제 게임 진행
+	while (1)
+	{
+		if (ret > 0)
+		{
+			//숫자 출력
+			for (i = 0; i < input; i++)
+			{
+				outputNumberU(myBingo[i], input);
+				printf("\b|\n");
+			}
+			printf("\n");
+			ret = inputNumber(myBingo, t, input, &num);
+		}
+		else if (ret == NULL)
+			ret = inputNumber(myBingo, t, input, &num);
+		printf("\n");
+
+		countLine = checkLine(myBingo, input);
+
+		if (countLine == input)
+			break;
+	}
+
+	deleteAllocI(myBingo, input);
+	free(myBingo);
+}
+
+void playConfrontation()
+{
+	int countLineU, countLineC, ret = 1, input, i, j, count[1000], t[1000];
+	int num = 0, retPrint;
+	char **myBingo, **computerBingo;
+
+	printf("# 빙고판의 가로,세로 크기를 입력해주세요(양수값 입력) : ");
+	scanf("%d", &input);
+	printf("\n");
+
+	myBingo = (char**)malloc(sizeof(char*) * input);
+	assert(myBingo != NULL);
+	computerBingo = (char**)malloc(sizeof(char*) * input);
+	assert(myBingo != NULL);
+
+	makeAllocI(myBingo, input);
+	makeAllocI(computerBingo, input);
+
+	//배열 초기화
+	for (i = 0; i < (input * input); i++)
+	{
+		count[i] = 0;
+		t[i] = 0;
+	}
+
+	//난수 생성 (User)
+	makeRandS(myBingo, count, input);
+	for (i = 0; i < (input * input); i++)
+	{
+		count[i] = 0;
+	}
+	makeRandS(computerBingo, count, input);
+
+	while (1)
+	{
+		if (ret > 0)
+		{
+			//숫자 출력
+			printf(" [User] \t\t [Computer]\n");
+			for (i = 0; i < input; i++)
+			{
+				outputNumberU(myBingo[i], input);
+				outputNumberC(computerBingo[i], input);
+			}
+			printf("\n");
+			ret = inputNumber(myBingo, t, input, &num);
+		}
+		else if (ret == NULL)
+		{
+			ret = inputNumber(myBingo, t, input, &num);
+		}
+		printf("\n");
+
+		for (i = 0; i < input; i++)
+		{
+			for (j = 0; j < input; j++)
+			{
+				if (computerBingo[i][j] == num)
+					computerBingo[i][j] = 'X';
+			}
+		}
+
+		printf(" [User] \t\t [Computer]\n");
+		for (i = 0; i < input; i++)
+		{
+			outputNumberU(myBingo[i], input);
+			outputNumberC(computerBingo[i], input);
+		}
+		printf("\n");
+
+		inputNumberC(myBingo, computerBingo, input, num);
+
+		//지워진 라인 체크
+		countLineU = checkLine(myBingo, input);
+		countLineC = checkLine(computerBingo, input);
+
+		retPrint = printResult(countLineU, countLineC, input);
+
+		if (retPrint == 1)
+			break;
+	}
+
+	printf("\n");
+
+	deleteAllocI(myBingo, input);
+	deleteAllocI(computerBingo, input);
+	free(myBingo);
+	free(computerBingo);
+}
+
 int checkLine(char **myBingo, int input)
 {
 	int i, j, count = 0;
@@ -76,7 +221,7 @@ int checkLine(char **myBingo, int input)
 		{
 			if (myBingo[i][j] != 'X')
 				break;
-			else if(j == input - 1)
+			else if (j == input - 1)
 				count++;
 		}
 	}
@@ -110,6 +255,28 @@ int checkLine(char **myBingo, int input)
 
 	return count;
 }
+
+int printfResult(int countLineU, int countLineC, int input)
+{
+	if ((countLineU == input) && (countLineC == input))
+	{
+		printf("# 사용자, 컴퓨터 무승부! \n");
+		return 1;
+	}
+	else if (countLineU == input)
+	{
+		printf("# 사용자 승! \n");
+		return 1;
+	}
+	else if (countLineC == input)
+	{
+		printf("# 컴퓨터 승! \n");
+		return 1;
+	}
+
+	return 0;
+}
+
 
 int inputNumber(char **myBingo, int *t, int input, int *num)
 {
@@ -169,161 +336,6 @@ void inputNumberC(char **myBingo, char **computerBingo, int input, int num)
 	}
 }
 
-void playIndividual()
-{
-	int i, j, input, ret = 1, countLine = 0, num;
-	static int count[1000], t[1000];
-	char **myBingo;
-
-	printf("# 빙고판의 가로,세로 크기를 입력해주세요(양수값 입력) : ");
-	scanf("%d", &input);
-	printf("\n");
-
-	myBingo = (char**)malloc(sizeof(char*) * input);
-	assert(myBingo != NULL);
-
-	makeAllocI(myBingo, input);
-
-	//배열 초기화
-	for (i = 0; i < (input * input); i++)
-	{
-		count[i] = 0;
-		t[i] = 0;
-	}
-
-	//난수 생성
-	makeRandS(myBingo, count, input);
-	printf("\n");
-
-	//실제 게임 진행
-	while (1)
-	{
-		if (ret > 0)
-		{
-			//숫자 출력
-			for (i = 0; i < input; i++)
-			{
-				outputNumberU(myBingo[i], input);
-				printf("\b|\n");
-			}
-			printf("\n");
-			ret = inputNumber(myBingo, t, input, &num);
-		}
-		else if (ret == NULL)
-			ret = inputNumber(myBingo, t, input, &num);
-		printf("\n");
-
-		countLine = checkLine(myBingo, input);
-
-		if (countLine == input)
-			break;
-	}
-
-	deleteAllocI(myBingo, input);
-	free(myBingo);
-}
-
-void playConfrontation()
-{
-	int countLineU, countLineC, ret = 1, input, i, j, count[1000], t[1000];
-	int num = 0;
-	char **myBingo, **computerBingo;
-
-	printf("# 빙고판의 가로,세로 크기를 입력해주세요(양수값 입력) : ");
-	scanf("%d", &input);
-	printf("\n");
-
-	myBingo = (char**)malloc(sizeof(char*) * input);
-	assert(myBingo != NULL);
-	computerBingo = (char**)malloc(sizeof(char*) * input);
-	assert(myBingo != NULL);
-
-	makeAllocI(myBingo, input);
-	makeAllocI(computerBingo, input);
-
-	//배열 초기화
-	for (i = 0; i < (input * input); i++)
-	{
-		count[i] = 0;
-		t[i] = 0;
-	}
-
-	//난수 생성 (User)
-	makeRandS(myBingo, count, input);
-	for (i = 0; i < (input * input); i++)
-	{
-		count[i] = 0;
-	}
-	makeRandS(computerBingo, count, input);
-
-	while (1)
-	{ 
-		if (ret > 0)
-		{
-			//숫자 출력
-			printf(" [User] \t\t [Computer]\n");
-			for (i = 0; i < input; i++)
-			{
-				outputNumberU(myBingo[i], input);
-				outputNumberC(computerBingo[i], input);
-			}
-			printf("\n");
-			ret = inputNumber(myBingo, t, input, &num);
-		}
-		else if (ret == NULL)
-		{
-			ret = inputNumber(myBingo, t, input, &num);
-		}
-		printf("\n");
-
-		for (i = 0; i < input; i++)
-		{
-			for (j = 0; j < input; j++)
-			{
-				if (computerBingo[i][j] == num)
-					computerBingo[i][j] = 'X';
-			}
-		}
-
-		printf(" [User] \t\t [Computer]\n");
-		for (i = 0; i < input; i++)
-		{
-			outputNumberU(myBingo[i], input);
-			outputNumberC(computerBingo[i], input);
-		}
-		printf("\n");
-
-		inputNumberC(myBingo, computerBingo, input, num);
-
-		//지워진 라인 체크
-		countLineU = checkLine(myBingo, input);
-		countLineC = checkLine(computerBingo, input);
-
-		if ((countLineU == input) && (countLineC == input))
-		{
-			printf("# 사용자, 컴퓨터 무승부! \n");
-			break;
-		}
-		else if (countLineU == input)
-		{
-			printf("# 사용자 승! \n");
-			break;
-		}
-		else if (countLineC == input)
-		{
-			printf("# 컴퓨터 승! \n");
-			break;
-		}
-	}
-
-	printf("\n");
-
-	deleteAllocI(myBingo, input);
-	deleteAllocI(computerBingo, input);
-	free(myBingo);
-	free(computerBingo);
-}
-
 
 void outputNumberU(char *Bingo, int input)
 {
@@ -360,21 +372,6 @@ void outputNumberC(char *Bingo, int input)
 }
 
 
-void makeRandS(char **Bingo, int *count, int input)
-{
-	int i, j;
-
-	for (i = 0; i < input; i++)
-	{
-		for (j = 0; j < input; j++)
-		{
-			Bingo[i][j] = makeRand(input * input, count);
-		}
-	}
-	printf("\n");
-}
-
-
 void makeAllocI(char **myBingo, int input)
 {
 	int i;
@@ -394,6 +391,20 @@ void deleteAllocI(char **myBingo, int input)
 		free(myBingo[i]);
 }
 
+
+void makeRandS(char **Bingo, int *count, int input)
+{
+	int i, j;
+
+	for (i = 0; i < input; i++)
+	{
+		for (j = 0; j < input; j++)
+		{
+			Bingo[i][j] = makeRand(input * input, count);
+		}
+	}
+	printf("\n");
+}
 
 int makeRand(int n, int *count)
 {
